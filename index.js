a78fb0f42f07aa910d0bc9b448e13f89587491c9
brunohuffenbaecher <bruno.henrique56@hotmail.com>
2026-06-11 13:48:11 -0300

    Criei uma função básica!

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 // 2 pessoas implementarão um código que:
 // 1) Cria uma variável que armazena um nome
 // 2) Mostra esse nome no console, no estilo "Hello, world!"
+
+let nome = "Bruno";
+
+console.log(`Hello, ${nome}!`);
