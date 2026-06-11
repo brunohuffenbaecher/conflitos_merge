65aaf3b55539a355d897abc31dff8adac8dc64e6
Gerson <honoriogerson01@gmail.com>
2026-06-11 13:46:10 -0300

    alterações do Jefferson

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Esse arquivo é apenas um teste
 // 2 pessoas implementarão um código que:
 // 1) Cria uma variável que armazena um nome
+let nome = "Asafe";
 // 2) Mostra esse nome no console, no estilo "Hello, world!"
+console.log(`Hello, ${nome}!`);
+
+//ou outro jeito mais simples
+console.log("Hello, " + nome + "!");
