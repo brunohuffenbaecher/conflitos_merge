e71792f8afb4560731f5d25475a820b46e25f84d
brunohuffenbaecher <bruno.henrique56@hotmail.com>
2026-06-10 21:18:46 -0300

    Commit inicial - Escrevi a estrutura inicial do projeto

---
+++ index.js
@@ -0,0 +1,4 @@
+// Esse arquivo é apenas um teste
+// 2 pessoas implementarão um código que:
+// 1) Cria uma variável que armazena um nome
+// 2) Mostra esse nome no console, no estilo "Hello, world!"
